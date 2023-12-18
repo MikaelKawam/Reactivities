55588aaa28ceb2bb9e0b952f20ad349393ffdb32
Mikael <mikaelkawam.dev@gmail.com>
2023-12-17 21:15:14 -0300

    feat: add ActivitiesController

--- Persistence/Migrations/20231217195228_InitialCreate.Designer.cs
+++ Persistence/Migrations/20231217195228_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using Persistence;
 namespace Persistence.Migrations
 {
     [DbContext(typeof(DataContext))]
-    [Migration("20231209225824_InitialCreate")]
+    [Migration("20231217195228_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
